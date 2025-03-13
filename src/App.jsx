59ccdc69bcb26e095633904a337f67aeea5a7a8c
Taranjeet-kaur <137863748+Taranjeet-kaur@users.noreply.github.com>
2025-03-13 19:55:40 +0000

    Added Test component featuring title, SVG, and input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Test(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <>
+    <h1>Test title</h1>
+    <svg>
+      <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+    </svg>
+    <form>
+      <input type="text" />
+    </form>
+  </>
   )
 }
 
-export default App
+export default Test;
